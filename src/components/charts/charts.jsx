0addd987101c5313ac0f132cdaf9c55b8096c87b
Terence Buchannan <terencebuchannan@gmail.com>
2018-04-18 15:55:10 -0400

    Charts: Refactored to stateless component

--- src/components/charts/charts.jsx
+++ src/components/charts/charts.jsx
@@ -3,34 +3,17 @@ import MaleFemaleChart from './maleFemaleChart';
 import FirstNamesChart from './firstNamesChart';
 import LastNamesChart from './lastNamesChart';
 
-class Charts extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      male: 0,
-      female: 0,
-      firstNameAM: 0,
-      lastNameAM: 0,
-      firstNameNZ: 0,
-      lastNameNZ: 0,
-      states: [],
-      ageRanges: [],
-      total: 0
-    }
-  }
-
-  render () {
+const Charts = props =>  {
+  const { info } = props;
     return (
-        <div> Chart Info
-          <MaleFemaleChart male={this.state.male} female={this.state.female} />
-          <FirstNamesChart firstNameAM={this.state.firstNameAM} firstNameNZ={this.state.firstNameNZ} />
-          <LastNamesChart lastNameAM={this.state.lastNameAM} lastNameNZ={this.state.lastNameNZ} />
-          <div> Total </div>
-          <div>{this.state.total}</div>
-        </div>
-
+      <div> Chart Info
+        <MaleFemaleChart male={info.male} female={info.female} />
+        <FirstNamesChart firstNameAM={info.firstNameAM} firstNameNZ={info.firstNameNZ} />
+        <LastNamesChart lastNameAM={info.lastNameAM} lastNameNZ={info.lastNameNZ} />
+        <div> Total </div>
+        <div>{info.total}</div>
+      </div>
     );
   }
-}
 
 export default Charts;
